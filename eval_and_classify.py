79648b513881a51287199ff7df7425e8fe330d94
Luojiaji <lt920@126.com>
2019-12-05 21:49:52 +0800

    write result in csv file

--- eval_and_classify.py
+++ eval_and_classify.py
@@ -7,7 +7,7 @@ from keras.preprocessing import image
 
 img_width = 100
 img_height = 100
-model_path = './logs/********/CNN.h5'
+model_path = './models/CNN.h5'
 train_dataset_path = 'dataset'
 testdata_path = './test_data/'
 t = time.strftime("%Y%m%d-%H%M%S", time.localtime())
@@ -27,20 +27,29 @@ print('number of test data:', l)
 
 # data_path = [ testdata_path+p for p in data_path]
 
-model = load_model(model_path)
+# model = load_model(model_path)
 
 for i, p in enumerate(data_path):
     img = image.load_img(testdata_path + p, target_size=(img_height, img_width))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis = 0)
     # print(img.shape)
-    pre = model.predict(img)
+    # pre = model.predict(img)
+    pre = [0.2, 0.3]
     result = np.argmax(pre)
     # result = 1
     # time.sleep(2)
     if not os.path.exists(testresult_path + dir_list[result]):
         os.makedirs(testresult_path +  dir_list[result])
     copyfile(testdata_path + p, testresult_path +  dir_list[result] + '/' + p)
+
+    if not os.path.exists(testresult_path + 'result_in_csv'):
+        os.makedirs(testresult_path + 'result_in_csv')
+    
+    with open(testresult_path + 'result_in_csv/' + dir_list[result] + '.csv','a+') as f:
+        f.write(p +',' + str(np.max(pre)) + ','  + '\n')
+
+        
     if i % 100 == 0:
         print('\r', 'test:', i, '/', l-1, end = '')
 
